13e7487c47598319e58610efa8062614fb4b2aaa
John <john.sarmiento452@gmail.com>
2024-02-09 10:10:44 +0800

    add database service

---
+++ app/app/tests.py
@@ -0,0 +1,21 @@
+""" Sample Test """
+
+from django.test import SimpleTestCase
+
+from . import calc
+
+
+class CalcTests(SimpleTestCase):
+    """ Test the calc module. """
+
+    def test_add_number(self):
+        """ Test adding numbers together. """
+        res = calc.add(5, 6)
+
+        self.assertEqual(res, 11)
+
+    def test_subtract_number(self):
+        """ Test subtracting number. """
+        res = calc.subtract(15, 10)
+
+        self.assertEqual(res, 5)
